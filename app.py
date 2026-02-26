214c1f4a33051bbd12240ab582c11eccc3b098e7
Shrikantlala24 <shrikantlala2023@gmail.com>
2026-02-26 21:33:24 +0530

    st trial 2

--- app.py
+++ app.py
@@ -4,3 +4,9 @@ import seaborn as sns
 
 import streamlit as st
 
+st.sidebar.checkbox("Show raw data", key="show_data")
+
+
+st.sidebar.radio("Select a plot type", (
+    "Medal Distribution", "Age Distribution", "Height vs Weight"
+    ), key="plot_type")
