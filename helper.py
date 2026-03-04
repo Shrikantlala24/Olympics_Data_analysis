c8b4d41a10b496fa4979621495f31e6b73bc95cc
Shrikantlala24 <shrikantlala2023@gmail.com>
2026-03-04 23:58:40 +0530

    Update helper.py

--- helper.py
+++ helper.py
@@ -4,20 +4,18 @@ df = None
 def medal(df_passed):
     global df, medal_tally, df2
     df = df_passed
+
     # summer.Sport.value_counts()
     df2 = df.drop_duplicates(subset=['Team', 'NOC','Games','Year','Season','City','Sport', 'Event', 'Medal'])
     # Team	NOC	Games	Year	Season	City	Sport	Event
     medal_tally = df2.groupby('region')[['Gold', 'Silver', 'Bronze']].sum().sort_values('Gold', ascending=False)
 
     # medal tally is df for medal tally of each country in each olympics
-
     medal_tally['Total'] = medal_tally['Gold'] + medal_tally['Silver'] + medal_tally['Bronze']
     return medal_tally
 
 def return_country_list():
     country =  ['Overall'] + medal_tally.index.sort_values().tolist()
-
-
     return country
 
 def return_year_list():
@@ -49,5 +47,8 @@ def fetch_Medal_tally(country,year):
     x['total'] = x['Gold'] + x['Silver'] + x['Bronze']
     return x
 
-
-# 
+# Overall analysis functions
+def Nations_per_year(df):
+    nations_per_year = df.drop_duplicates(['Year', 'region'])['Year'].value_counts().sort_index().reset_index()
+    nations_per_year.rename(columns={ 'nations': 'Year', 'count': 'nations' }, inplace=True)
+    return nations_per_year
